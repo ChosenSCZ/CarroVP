0bfa692625f2736aec643ce1ff848390f67ff046
ChosenSCZ <christoffer.espinozah@gmail.com>
2024-03-27 23:46:16 -0400

    5

---
+++ products.js
@@ -0,0 +1,131 @@
+const products = [
+    {
+        "id": 1,
+        "name": "BARF Guau! 1Kg",
+        "flavor": "Res",
+        "description": "1Kg de delicioso Alimento BARF Guau! sabor Res",
+        "price": 38,
+        "image": "images/barf-guau-res.jpg"
+    },  
+    {
+        "id": 2,
+        "name": "BARF Guau! 5Kg",
+        "flavor": "Res",
+        "description": "5Kg de delicioso Alimento BARF Guau! sabor Res",
+        "price": 185,
+        "image": "images/barf-guau-res.jpg"
+    },  
+    {
+        "id": 3,
+        "name": "BARF Guau! 10Kg",
+        "flavor": "Res",
+        "description": "10Kg de delicioso Alimento BARF Guau! sabor Res",
+        "price": 360,
+        "image": "images/barf-guau-res.jpg"
+    },
+    {
+        "id": 4,
+        "name": "BARF Guau! 1Kg",
+        "flavor": "Pollo",
+        "description": "1Kg de delicioso Alimento BARF Guau! sabor Pollo",
+        "price": 36,
+        "image": "images/barf-guau-pollo.jpg"
+    },
+    {
+        "id": 5,
+        "name": "BARF Guau! 5Kg",
+        "flavor": "Pollo",
+        "description": "5Kg de delicioso Alimento BARF Guau! sabor Pollo",
+        "price": 175,
+        "image": "images/barf-guau-pollo.jpg"
+    },
+    {
+        "id": 6,
+        "name": "BARF Guau! 10Kg",
+        "flavor": "Pollo",
+        "description": "10Kg de delicioso Alimento BARF Guau! sabor Pollo",
+        "price": 340,
+        "image": "images/barf-guau-pollo.jpg"
+    },
+    {
+        "id": 7,
+        "name": "BARF Miau! 1Kg",
+        "flavor": "Res",
+        "description": "1Kg de delicioso Alimento BARF Miau! sabor Res",
+        "price": 40,
+        "image": "images/barf-miau-res.jpg"
+    },
+    {
+        "id": 8,
+        "name": "BARF Miau! 3Kg",
+        "flavor": "Res",
+        "description": "3Kg de delicioso Alimento BARF Miau! sabor Res",
+        "price": 117,
+        "image": "images/barf-miau-res.jpg"
+    },
+    {
+        "id": 9,
+        "name": "BARF Miau! 5Kg",
+        "flavor": "Res",
+        "description": "5Kg de delicioso Alimento BARF Miau! sabor Res",
+        "price": 190,
+        "image": "images/barf-miau-res.jpg"
+    },
+    {
+        "id": 10,
+        "name": "BARF Miau! 1Kg",
+        "flavor": "Pollo",
+        "description": "1Kg de delicioso Alimento BARF Miau! sabor Pollo",
+        "price": 38,
+        "image": "images/barf-miau-pollo.jpg"
+    },
+    {
+        "id": 11,
+        "name": "BARF Miau! 3Kg",
+        "flavor": "Pollo",
+        "description": "3Kg de delicioso Alimento BARF Miau! sabor Pollo",
+        "price": 110,
+        "image": "images/barf-miau-pollo.jpg"
+    },
+    {
+        "id": 12,
+        "name": "BARF Miau! 5Kg",
+        "flavor": "Pollo",
+        "description": "5Kg de delicioso Alimento BARF Miau! sabor Pollo",
+        "price": 180,
+        "image": "images/barf-miau-pollo.jpg"
+    },
+    {
+        "id": 13,
+        "name": "GummyPet",
+        "flavor": "Pequeño",
+        "description": "14 porciones pequeñas de gomitas de colageno 100% naturales",
+        "price": 30,
+        "image": "images/gummypetp.jpg"
+    },
+    {
+        "id": 14,
+        "name": "GummyPet",
+        "flavor": "Grande",
+        "description": "7 porciones grandes de gomitas de colageno 100% naturales",
+        "price": 30,
+        "image": "images/gummypetg.jpg"
+    },
+    {
+        "id": 15,
+        "name": "GummyPet 4x3",
+        "flavor": "Pequeño",
+        "description": "56 porciones pequeñas de gomitas de colageno 100% naturales",
+        "price": 90,
+        "image": "images/gummypetp.jpg"
+    },
+    {
+        "id": 16,
+        "name": "GummyPet 4x3",
+        "flavor": "Grande",
+        "description": "28 porciones grandes de gomitas de colageno 100% naturales",
+        "price": 90,
+        "image": "images/gummypetg.jpg"
+    }
+];
+export default products;
